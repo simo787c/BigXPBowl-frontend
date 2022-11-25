c2361be066a105df26df10c19c5331cb960e0dfc
Thom <92270696+Thom719c@users.noreply.github.com>
2022-11-25 13:56:41 +0100

    Slider
    Updated tiny-slider style

--- src/static/js/userSys/getLanes.js
+++ src/static/js/userSys/getLanes.js
@@ -1,26 +1,19 @@
 class LanesRenderer {
 
-    constructor(data) {
-        this.data = data;
-        this.getData();
+    constructor() {
     }
 
-    async getData () {
-        this.data = await utilFetch.operationData("activities/bowling","","","GET");
-        //this.updateUI();
-    }
-
-    updateUI(){
+    async updateUI(){
         let lanes = document.getElementById("lanes");
 
-        this.getData();
+        let data = await utilFetch.operationData("activities/bowling","","","GET");
         
         try {
             //clear lanes div content
             lanes.innerHTML = ""
 
             //iterate through each lane, then clone and assign a htmltemplate for it
-            this.data.forEach(element => {
+            data.forEach(element => {
                 let clone = cloneHtmlTemplate("template-lane")
                 let laneNr = clone.querySelector("#laneNr")
                 let desc = clone.querySelector("#description")
