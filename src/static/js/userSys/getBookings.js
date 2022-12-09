45eb5fcaff42c401e28607333598823a3583ac17
Thom <92270696+Thom719c@users.noreply.github.com>
2022-12-09 09:41:45 +0100

    Update getBookings.js

--- src/static/js/userSys/getBookings.js
+++ src/static/js/userSys/getBookings.js
@@ -21,7 +21,7 @@ class BookingsRenderer {
                 clone.querySelector(".activityDate").innerHTML += element.activityDate.split("T")[0]
                 clone.querySelector(".activityDuration").innerHTML += element.activityDuration
                 if (element.bowlingTimeSlotJoinedTableList != "") {
-                    for (let i = 0; i < element.bowlingTimeSlotJoinedTableList.length; i++) {
+                    /*for (let i = 0; i < element.bowlingTimeSlotJoinedTableList.length; i++) {
                         clone.querySelector(".bowlingId").innerHTML += element.bowlingTimeSlotJoinedTableList[i].bowlingId + "<br>"
                         // Syntax for Get
                         let bowlingData = await utilFetch.operationData("activities/bowling/", element.bowlingTimeSlotJoinedTableList[i].timeSlotId, "", "GET");
@@ -31,11 +31,11 @@ class BookingsRenderer {
                         for (let j = 0; j < timeslot.length; j++) {
                             if (element.bowlingTimeSlotJoinedTableList[i].timeSlotId == timeslot[j].timeSlotId) {
                                 clone.querySelector(".timeSlot").innerHTML += timeslot[j].startTime  + "<br>"
-
                             }
                         }
-
-                    }
+                    }*/
+                    this.activityUI(clone, element, element.bowlingTimeSlotJoinedTableList)
+                    this.activityUI(clone, element, element.airHockeyTimeSlotJoinedTableList)
                 }
 
                 bookings.appendChild(clone)
@@ -43,6 +43,30 @@ class BookingsRenderer {
 
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    async activityUI(clone, element, activityJT) {
+        let activityId;
+        let activityType;
+        for (let i = 0; i < activityJT.length; i++) {
+            if (element.bowlingTimeSlotJoinedTableList != "") {
+                activityId = activityJT[i].bowlingId
+                activityType = "activities/bowling/"
+            } else if (element.airHockeyTimeSlotJoinedTableList != "") {
+                activityId = activityJT[i].airHockeyId
+                activityType = "activities/airHockey/"
+            }
+            clone.querySelector(".activityId").innerHTML += activityId + "<br>"
+            // Syntax for Get
+            let activityData = await utilFetch.operationData(activityType, activityJT[i].timeSlotId, "", "GET");
+            clone.querySelector(".activityType").innerHTML += await activityData.name + "<br>"
+            let timeslot = await utilFetch.operationData("timeSlots/", "", "", "GET");
+            for (let j = 0; j < timeslot.length; j++) {
+                if (activityJT[i].timeSlotId == timeslot[j].timeSlotId) {
+                    clone.querySelector(".timeSlot").innerHTML += timeslot[j].startTime  + "<br>"
+                }
+            }
         }
     }
 
