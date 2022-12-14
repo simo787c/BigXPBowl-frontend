077c3074f4a22397bd8fec266832f5c3c82597ad
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2022-12-14 11:12:29 +0100

    added create and update
    added create update in productItem.html

--- src/static/js/adminSys/ProductItemService.js
+++ src/static/js/adminSys/ProductItemService.js
@@ -4,7 +4,7 @@ class ProductItemRenderer {
     }
 
     async updateUI() {
-        let productItems = document.getElementById("productItems");
+        let productItemTable = document.getElementById("productItem-table");
 
         let data = await utilFetch.operationData("product/productItems", "", "", "GET");
         document.getElementById("view").setAttribute("class", "")
@@ -12,23 +12,23 @@ class ProductItemRenderer {
         console.log(data);
         try {
             //clear equipments div content
-            productItems.innerHTML = ""
+            productItemTable.innerHTML = ""
 
             //iterate through each item, then clone and assign a htmltemplate for it
             data.forEach(element => {
-                let clone = cloneHtmlTemplateTableTr("template-product-item")
+                let clone = cloneHtmlTemplateTableTr("template-productItem-table")
 
-                clone.querySelector(".productItemId").innerHTML += element.productItemId;
-                clone.querySelector(".name").innerHTML += element.name;
-                clone.querySelector(".price").innerHTML += element.price;
-                clone.querySelector(".productType").innerHTML += element.productType;
-                clone.querySelector(".description").innerHTML += element.description;
-                clone.querySelector(".deleteButton").value += element.productItemId;
-                clone.querySelector(".editButton").value += element.productItemId;
+/*                 clone.querySelector(".productItemId").innerHTML += element.productItemId;
+ */                clone.querySelector(".productItem-name").innerHTML += element.name;
+                clone.querySelector(".productItem-price").innerHTML += element.price;
+                clone.querySelector(".productItem-productType").innerHTML += element.productType;
+                clone.querySelector(".productItem-description").innerHTML += element.description;
+                clone.querySelector(".btn-delete-productItem").value += element.productItemId;
+                clone.querySelector(".btn-edit-productItem").value += element.productItemId;
                 //for deleteProductItem() to delete the tr when deleting
-                clone.setAttribute("id", element.productItemId);
+                clone.setAttribute("id", "productItem" + element.productItemId);
 
-                productItems.appendChild(clone)
+                productItemTable.appendChild(clone)
             });
 
         } catch (error) {
@@ -36,11 +36,76 @@ class ProductItemRenderer {
         }
     }
 
+    openCreateProductItemModal() {
+        $(".btn-productItem-modal").val("Opret")
+        $(".btn-productItem-modal").attr("onclick", "productItemRenderer.createProductItem()")
+        document.getElementById("productItemName").value = "";
+        document.getElementById("productItemPrice").value = "";
+        document.getElementById("productItemProductType").value = "";
+        document.getElementById("productItemDescription").value = "";
+    }
+
+    async createProductItem() {
+        let name = document.getElementById("productItemName").value;
+        let price = document.getElementById("productItemPrice").value;
+        let productType = document.getElementById("productItemProductType").value;
+        let description = document.getElementById("productItemDescription").value;
+
+        const productItem = new Map([
+            ['name', name],
+            ['price', price],
+            ['productType', productType],
+            ['description', description],
+        ])
+        const productItemData = Object.fromEntries(productItem);
+
+        await utilFetch.operationData("product/productItem/", "", productItemData, "POST");
+
+        this.updateUI();
+
+        $('#productItemModal').modal('hide');
+    }
+
+    async openEditProductItem(id) {
+        let data = await utilFetch.operationData("product/productItem/", id, "", "GET");
+        $(".btn-productItem-modal").attr("onclick", "productItemRenderer.editProductItem()")
+
+        $(".btn-productItem-modal").val("Gem")
+
+        this.productItemId = id;
+        document.getElementById("productItemName").value = data.name;
+        document.getElementById("productItemPrice").value = data.price;
+        document.getElementById("productItemProductType").value = data.productType;
+        document.getElementById("productItemDescription").value = data.description;
+    }
+
+    async editProductItem() {
+        let productItemName = document.getElementById("productItemName").value;
+        let productItemPrice = document.getElementById("productItemPrice").value;
+        let productItemProductType = document.getElementById("productItemProductType").value;
+        let productItemDescription = document.getElementById("productItemDescription").value;
+        const productItem = new Map([
+            ['productItemId', this.productItemId],
+            ['name', productItemName],
+            ['price', productItemPrice],
+            ['productType', productItemProductType],
+            ['description', productItemDescription],
+        ])
+        const productItemData = Object.fromEntries(productItem);
+        console.log(productItemData)
+
+        await utilFetch.operationData(`product/productItem/`, this.productItemId, productItemData, "PATCH");
+
+        this.updateUI();
+
+        $('#productItemModal').modal('hide');
+    }
+
     deleteProductItem(id) {
         if (this.confirmDelete()) {
             utilFetch.operationData("product/productItem/", id, "", "DELETE");
             //Remove deleted element from UI
-            $('#' + id).remove();
+            $('#productItem' + id).remove();
             console.log('Delete was successful');
         } else {
             console.log('Delete was cancelled');
@@ -50,71 +115,7 @@ class ProductItemRenderer {
     confirmDelete() {
         return confirm('Er du sikker på du vil slette?');
     }
-
-    async editButtonClick(id) {
-        console.log("edit clicked " + id);
-        //$('#formPost').attr('method','PATCH')
-        document.getElementById("productItemMethodType").value = "PATCH";
-        document.getElementById("submitButton").value = "Gem";
-        //console.log(document.querySelector("#formPost").method);
-        let data = await utilFetch.operationData("product/productItem/", id, "", "GET");
-
-        document.getElementById("productItemId").value = data.productItemId;
-        document.getElementById("name").value = data.name;
-        document.getElementById("price").value = data.price;
-        document.getElementById("productType").value = data.productType;
-        document.getElementById("description").value = data.description;
-        
-        if(data.productType == "Beverages"){
-            document.getElementById("productType").value = "Beverages"
-        }else if(data.productType == "Food"){
-            document.getElementById("productType").value = "Food"
-        }
-    }
-
-    createButtonClick() {
-        console.log("create clicked");
-        document.getElementById("submitButton").value = "Tilføj";
-        //$('#formPost').attr('method','POST')
-        document.getElementById("productItemMethodType").value = "POST";
-        //console.log(document.querySelector("#formPost").method);
-    }
 }
-
 var productItemRenderer = new ProductItemRenderer;
 
-const formPostEvent = document.querySelector("#formPost");
-
-// listening to when Post form get submitted
-formPostEvent.addEventListener("submit", async (event) => {
-    event.preventDefault();
-    let productItemMethodType = document.getElementById("productItemMethodType").value
-
-    const formData = new FormData(formPostEvent);
-
-    //we made a workaround to use a field in form with the method type, as method attribute on form is dumb
-    // We can write method type in uppercase but it gives in lowercase (deprecated)
-    //in the html form, it only has post and get, we dont use the functionality (deprecated)
-    // if (equipmentMethodType == "POST") {
-    //     console.log("IF " + equipmentMethodType);
-    //     formData.delete("id");
-    //     dataFromForm = Object.fromEntries(formData);
-    //     await utilFetch.operationData("equipment", "", dataFromForm, "POST");
-    // } else if (equipmentMethodType == "PATCH") {
-    //     console.log("IF " + equipmentMethodType);
-    //     dataFromForm = Object.fromEntries(formData);
-    //     await utilFetch.operationData("equipment/", document.getElementById("id").value, dataFromForm, "PATCH");
-    // }
-
-    //NEW NEW post patch method, fetch doesnt need an ID by itself since if its included in the form it'll know what to do
-    //Might be a security breach since its done in the frontend, and post endpoint doesnt break if it gets an id with the request
-    //5. in https://www.baeldung.com/spring-data-crud-repository-save
-    if (productItemMethodType == "POST") {
-        console.log("IF " + productItemMethodType);
-        formData.delete("id");
-    }
-    const dataFromForm = Object.fromEntries(formData);
-    await utilFetch.operationData("product/productItem/", "", dataFromForm, "POST");
-    productItemRenderer.updateUI();
-})
 
