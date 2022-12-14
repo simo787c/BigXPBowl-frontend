8213c9ba0c636d569b93c63b1dbe7966abfeeb94
simo787c <92300326+simo787c@users.noreply.github.com>
2022-12-14 10:32:12 +0100

    updated order

--- src/static/js/userSys/order.js
+++ src/static/js/userSys/order.js
@@ -1,34 +1,26 @@
-class Orderrenderer {
+class OrderRenderer {
 
     constructor() {
-        this.method();
     }
 
     async updateUI() {
-        let equipments = document.getElementById("equipments");
+        let equipments = document.getElementById("productItems");
 
-        let data = await utilFetch.operationData("equipment", "", "", "GET");
-        document.getElementById("view").setAttribute("class", "")
-        document.getElementsByTagName("")
+        let data = await utilFetch.operationData("product/productItems", "", "", "GET");
         console.log(data);
         try {
             //clear equipments div content
             equipments.innerHTML = ""
 
-            //iterate through each lane, then clone and assign a htmltemplate for it
+            //iterate through each, then clone and assign a htmltemplate for it
             data.forEach(element => {
                 //id,activityType,name,description,condition
-                let clone = cloneHtmlTemplateTableTr("template-equipment-item")
+                let clone = cloneHtmlTemplateTableTr("template-order-productItem")
 
-                clone.querySelector(".id").innerHTML += element.id;
-                clone.querySelector(".activityType").innerHTML += element.activityType;
                 clone.querySelector(".name").innerHTML += element.name;
                 clone.querySelector(".description").innerHTML += element.description;
-                clone.querySelector(".condition").innerHTML += element.condition;
-                clone.querySelector(".deleteButton").value += element.id;
-                clone.querySelector(".editButton").value += element.id;
-                //for deleteEquipment() to delete the tr when deleting
-                clone.setAttribute("id", element.id);
+                clone.querySelector(".productType").innerHTML += element.productType;
+                clone.querySelector(".price").innerHTML += element.price;
 
                 equipments.appendChild(clone)
             });
@@ -37,5 +29,9 @@ class Orderrenderer {
             console.log(error)
         }
     }
+
+    //when enter is pressed while targeting a textbox on a table item
+    //append the item data + the textbox value into the cart div as a new row
+    //multiply price with quantity given in textbox
 }
-var orderrenderer = new Orderrenderer;
+var orderRenderer = new OrderRenderer;
